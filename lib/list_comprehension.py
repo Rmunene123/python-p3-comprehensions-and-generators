b61032e7a3f67a85b54c6483b138c4a3fb8a1009
Rmunene123 <rosewanmun@gmail.com>
2024-09-29 00:49:25 +0300

    added c & g

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """Return a list of all even elements from the input list of integers."""
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    """Return a list of sentences with exclamation marks at the end."""
+    return [sentence + "!" for sentence in sentence_list]
